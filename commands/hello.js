9873fab475ab642c329413e7efe72e573bd64fd1
cmdrGuyson <gayangakuruppu@gmail.com>
2020-03-25 16:18:21 +0530

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "Hello!",
-  aliases: ["hello", "identify"],
+  aliases: ["hello", "identify", "hi"],
   description: "Basic identification and greeting.",
 
   execute(message, args) {
@@ -14,7 +14,7 @@ module.exports = {
         "Hello! I'm Aether. I'm a discord bot that will give you will give you updated details on emergency situations happening around the world."
       )
       .setThumbnail("https://i.imgur.com/2w4CF8P.png")
-      .addField("For help", "use: '>aether help'", true)
+      .addField("For help", "use: `>aether help`", true)
       .setFooter("Created by Guyson and Neith");
 
     message.channel.send(embed);
