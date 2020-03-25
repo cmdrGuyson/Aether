69c873303dfa1ae4b0abba46e873a434ea775bc9
NethmiRodrigo <34070216+NethmiRodrigo@users.noreply.github.com>
2020-03-25 19:05:43 +0530

    Added sri lanka and global

--- commands/covid19.js
+++ commands/covid19.js
@@ -25,7 +25,7 @@ module.exports = {
     collector.on("end", async collected => {
       console.log(collected.size);
 
-      if (country != "sri-lanka") {
+      if (country != "sri-lanka" && country != "global") {
         axios
           .get("https://api.covid19api.com/summary")
           .then(data => {
@@ -77,7 +77,7 @@ module.exports = {
                     Math.round(
                       ((result.TotalRecovered / result.TotalConfirmed) * 100 +
                         Number.EPSILON) *
-                        100
+                      100
                     ) / 100
                 },
                 {
@@ -86,11 +86,136 @@ module.exports = {
                     Math.round(
                       ((result.TotalDeaths / result.TotalConfirmed) * 100 +
                         Number.EPSILON) *
-                        100
+                      100
                     ) / 100
                 }
               )
               .setFooter(`Last updated on: ${data.data.Date}`);
+
+            message.channel.send(embed);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+      else if (country === "sri-lanka") {
+        axios
+          .get("https://www.hpb.health.gov.lk/api/get-current-statistical")
+          .then(data => {
+            let result = data.data.data;
+
+            const embed = new Discord.MessageEmbed()
+              .setColor("#ffbe00")
+              .setTitle(`Covid-19 | Sri Lanka`)
+              .setDescription("Please find the statistics below.")
+              .setThumbnail("https://i.imgur.com/2w4CF8P.png")
+              .addFields(
+                {
+                  name: "Total Confirmed",
+                  value: result["local_total_cases"],
+                  inline: true
+                },
+                {
+                  name: "New Confirmed",
+                  value: result["local_new_cases"],
+                  inline: true
+                },
+                {
+                  name: "Total Deaths",
+                  value: result["local_deaths"],
+                  inline: true
+                },
+                {
+                  name: "Total Recovered",
+                  value: result["local_recovered"],
+                  inline: true
+                },
+                { name: "\u200B", value: "\u200B" },
+                {
+                  name: "Recovered Percentage",
+                  value:
+                    Math.round(
+                      ((parseInt(result["local_recovered"], 10) / parseInt(result["local_total_cases"], 10)) * 100 +
+                        Number.EPSILON) *
+                      100
+                    ) / 100
+                },
+                {
+                  name: "Deaths Percentage",
+                  value:
+                    Math.round(
+                      ((parseInt(result["local_deaths"], 10) / parseInt(result["local_total_cases"], 10)) * 100 +
+                        Number.EPSILON) *
+                      100
+                    ) / 100
+                }
+              )
+              .setFooter(`Last updated on: ${result["update_date_time"]}`);
+
+            message.channel.send(embed);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+      else if (country === "global") {
+        axios
+          .get("https://www.hpb.health.gov.lk/api/get-current-statistical")
+          .then(data => {
+            let result = data.data.data;
+
+            const embed = new Discord.MessageEmbed()
+              .setColor("#ffbe00")
+              .setTitle(`Covid-19 | Global`)
+              .setDescription("Please find the statistics below.")
+              .setThumbnail("https://i.imgur.com/2w4CF8P.png")
+              .addFields(
+                {
+                  name: "Total Confirmed",
+                  value: result["global_total_cases"],
+                  inline: true
+                },
+                {
+                  name: "New Confirmed",
+                  value: result["global_new_cases"],
+                  inline: true
+                },
+                {
+                  name: "Total Deaths",
+                  value: result["global_deaths"],
+                  inline: true
+                },
+                {
+                  name: "New Deaths",
+                  value: result["global_new_deaths"],
+                  inline: true
+                },
+                {
+                  name: "Total Recovered",
+                  value: result["global_recovered"],
+                  inline: true
+                },
+                { name: "\u200B", value: "\u200B" },
+                {
+                  name: "Recovered Percentage",
+                  value:
+                    Math.round(
+                      ((parseInt(result["global_recovered"], 10) / parseInt(result["global_total_cases"], 10)) * 100 +
+                        Number.EPSILON) *
+                      100
+                    ) / 100
+                },
+                {
+                  name: "Deaths Percentage",
+                  value:
+                    Math.round(
+                      ((parseInt(result["global_deaths"], 10) / parseInt(result["global_total_cases"], 10)) * 100 +
+                        Number.EPSILON) *
+                      100
+                    ) / 100
+                }
+              )
+              .setFooter(`Last updated on: ${result["update_date_time"]}`);
 
             message.channel.send(embed);
           })
